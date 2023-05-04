c694a7707d6c8c0316de928eb01dd98ee355c84e
Claudia Hirose <claudiahirose@gmail.com>
2023-05-04 06:54:29 -0300

    Desafio Criando o Seu Blog Pessoal com Next.js

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://wrqseiduxeqrcjikpugz.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndycXNlaWR1eGVxcmNqaWtwdWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI4MDIwOTksImV4cCI6MTk5ODM3ODA5OX0.GuvZ440InD9rS4U_WVZjzhFl6Fm6tWnD5qpOyLrMeRk",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndycXNlaWR1eGVxcmNqaWtwdWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI4MDIwOTksImV4cCI6MTk5ODM3ODA5OX0.GuvZ440InD9rS4U_WVZjzhFl6Fm6tWnD5qpOyLrMeRk"
     }
 })
